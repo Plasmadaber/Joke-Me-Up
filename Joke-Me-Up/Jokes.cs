98e0d758f81fdb479756b84f001d717b0f4b4d0f
Channing Monsch <plasmastudioscontact@gmail.com>
2024-10-25 15:26:17 -0400

    Implements Get Joke function - Creates static class Jokes and calls the initilization function in MainWindow.axml.cs

--- Joke-Me-Up/Jokes.cs
+++ Joke-Me-Up/Jokes.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 namespace JokeMeUp
 {
-	public class Jokes
+	public static class Jokes
 	{
-		public Jokes()
+		private static List<string> _jokes = new List<string>();
+
+		public static void init()
 		{
+			StreamReader reader = new StreamReader("shortjokes.csv");
+
+			string line = reader.ReadLine();
+
+			while (line != null && line.Length != 0)
+			{
+				line = reader.ReadLine();
+				line = line.Substring(line.IndexOf(","));
+				_jokes.Add(line);
+			}
+		}
+
+		public static string GetJoke()
+		{
+			Random rand = new Random();
+			return _jokes[rand.Next(0, _jokes.Count)];
 		}
 	}
 }
